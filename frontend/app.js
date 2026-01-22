05c6bc558d4d1f715922bc37c3ada3eaa947d485
w53434129-cmyk <w53434129@gmail.com>
2026-01-22 15:51:09 +0500

    Add checkBackend function to fetch API health status

---
+++ frontend/app.js
@@ -0,0 +1,10 @@
+function checkBackend() {
+  fetch("http://3.89.212.211:3000/api/health")
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("result").innerText = data.status;
+    })
+    .catch(() => {
+      document.getElementById("result").innerText = "Backend not reachable";
+    });
+}
