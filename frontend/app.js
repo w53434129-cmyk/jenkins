b71d6964f5ec4eb853dce2fe5d602562b64510bc
w53434129-cmyk <w53434129@gmail.com>
2026-01-22 17:33:24 +0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 function checkBackend() {
-  fetch("http://localhost:3000/api/health")
+  fetch("http://localhost:3.89.212.211/api/health")
     .then(res => res.json())
     .then(data => {
       document.getElementById("result").innerText = data.status;
