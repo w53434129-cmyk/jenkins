107a4cc510938b78f99e94bc9bc602bfb846d6e8
w53434129-cmyk <w53434129@gmail.com>
2026-01-23 10:57:20 +0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,10 @@
 function checkBackend() {
-  fetch("http://3.89.212.211:3000/api/health")
+  fetch("/api/health")
     .then(res => res.json())
     .then(data => {
       document.getElementById("result").innerText = data.status;
     })
     .catch(() => {
-      document.getElementById("result").innerText = "Backend is running";
+      document.getElementById("result").innerText = "Backend not reachable";
     });
 }
